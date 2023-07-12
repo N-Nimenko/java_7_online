3e14b8a3830070d5dc6c255d2cbf4983c5dcfe45
Nikita Nimenko <120600002+N-Nimenko@users.noreply.github.com>
2023-07-12 19:39:59 +0300

    Add files via upload

---
+++ homework7/src/main/java/homework7/Main.java
@@ -0,0 +1,10 @@
+package homework7;
+
+import homework7.controller.Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
