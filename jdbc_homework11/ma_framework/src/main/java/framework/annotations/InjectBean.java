24b631efe753254b138132950a5b6d52a5cc2a0f
Nikita Nimenko <120600002+N-Nimenko@users.noreply.github.com>
2023-08-20 12:04:24 +0300

    Delete jdbc_homework11/ma_framework directory

--- jdbc_homework11/ma_framework/src/main/java/framework/annotations/InjectBean.java
+++
@@ -1,8 +0,0 @@
-package framework.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface InjectBean { }
-//Если вы хотите внедрить BeanClass в BeanClass, - объявите в нужном вам классе.
